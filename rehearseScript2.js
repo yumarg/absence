33500303b8e382cc5871d6d6ea9cbee8b7c66b6b
LantianChen <lantian@mit.edu>
2017-04-18 12:03:30 -0400

    Rehearse Script pages

---
+++ rehearseScript2.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+	var allIcons = document.getElementsByClassName("icon");
+    for (var icon = 0; icon < allIcons.length; icon++) {
+      allIcons[icon].style.color = themeColors.regular;
+    }
+
+	$(".actions div").hover(
+		function() {
+			$(this).css("border", "2px solid " + themeColors.regular);
+		},
+		function() {
+			$(this).css("border", "none");
+		}
+	);
+
+	$('.nextLine').click(function() {
+    	window.location.href = "rehearseScript4.html";
+	});
+
+	$('.lineButton').click(function() {
+    	window.location.href = "rehearseScript3.html";
+	});
+
+});
+
+
+function enablePlay() {
+	$(".play i").removeClass("disabled");
+	$(".play i").addClass("regular");	
+}
+
+function disablePlay() {
+	$(".play").removeClass("highlighted");
+	$(".play i").removeClass("regular");	
+	$(".play i").addClass("disabled");
+}
+
+function enableSave() {
+	$(".save i").removeClass("disabled");
+	$(".save i").addClass("regular");
+}
+
+function disableSave() {
+	$(".save").removeClass("highlighted");
+	$(".save i").removeClass("regular");	
+	$(".save i").addClass("disabled");	
+}

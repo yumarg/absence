e4c126022df41ed61e6e895ea1b41c5c95e7f768
Margaret Yu <margyu@mit.edu>
2017-05-08 18:14:13 -0400

    added home button

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 var themeColors = {regular: "#610996", highlighted: "#be9ad3", disabled: "#838383"};
 var themeFonts = {regular: "Lato"};
 var saved = false;
+
+$(document).ready(function() {
+	$("#home").hover(
+		function() {
+			$("#home").html("<i class='regular fa fa-file-text-o' aria-hidden='true'></i><span style='color: #000; font-size: 20px'>&nbsp; Back to Script Menu</span>");
+			$("#home").animate({"width": "250px"});	
+			$("#home").css("opacity", 0.9);		
+		},
+		function() {			
+			$("#home").html("<i class='regular fa fa-file-text-o' aria-hidden='true'></i>");
+			$("#home").animate({"width": "140px"});			
+		}
+	);
+});
